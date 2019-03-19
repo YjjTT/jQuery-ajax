e4b5c94fe21b753547c342f26be0c52e4545f9ec
YjjTT <yjjtt@tempot.com>
2019-03-19 18:08:42 +0800

    update

--- main.js
+++ main.js
@@ -1,43 +1,50 @@
-window.jQuery = function (nodeOrSelector) {
+window.jQuery = function(nodeOrSelector){
     let nodes = {}
-    nodes.addClass = function () {}
+    nodes.addClass = function(){}
+    nodes.html = function(){}
     return nodes
-}
-
-window.jQuery.ajax = function ({
-    url,
-    method,
-    body,
-    successFn,
-    failFn,
-    headers
-}) {
-    return new Promise(function (resolve, reject) {
-        let request = new XMLHttpRequest()
-        request.open(method, url)
-        for (let key in headers) {
-            let value = headers[key]
-            request.setRequestHeader(key, value)
+  }
+  window.$ = window.jQuery
+  
+  window.jQuery.ajax = function({url, method, body, successFn, failFn, headers}){
+    let request = new XMLHttpRequest()
+    request.open(method, url) // 配置request
+    for(let key in headers) {
+      let value = headers[key]
+      request.setRequestHeader(key, value)
+    }
+    request.onreadystatechange = ()=>{
+      if(request.readyState === 4){
+        if(request.status >= 200 && request.status < 300){
+          successFn.call(undefined, request.responseText)
+        }else if(request.status >= 400){
+          failFn.call(undefined, request)
         }
-        request.onreadystatechange = () => {
-            if (request.status >= 200 && request.status <= 300) {
-                resolve.call(undefined, request.responseText)
-            } else {
-                reject.call(undefined, request)
-            }
-        }
-        request.send(body)
+      }
+    }
+    request.send(body)
+  }
+  
+  function f1(responseText){}
+  function f2(responseText){}
+  
+  myButton.addEventListener('click', (e)=>{
+    window.jQuery.ajax({
+      url: '/frank',
+      method: 'get',
+      headers: {
+        'content-type':'application/x-www-form-urlencoded',
+        'frank': '18'
+      },
+      successFn: (x)=>{
+        f1.call(undefined,x)
+        f2.call(undefined,x)
+      },
+      failFn: (x)=>{
+        console.log(x)
+        console.log(x.status)
+        console.log(x.responseText)
+      }
     })
-}
-// function success(responseText){
-//     console.log(responseText)
-// }
-// function fail (request){
-//     console.log(request)
-// }
-// myButton.addEventListener('click', function(e){
-//     window.jQuery.ajax({
-//         url: '/xxx',
-//         method: 'post',
-//     }).then(success, fail)
-// })
+  })
+  
---
+++ pormise.js
@@ -0,0 +1,38 @@
+window.jQuery.ajax = function({url, method, body, headers}){
+    return new Promise(function(resolve, reject){
+      let request = new XMLHttpRequest()
+      request.open(method, url) // 配置request
+      for(let key in headers) {
+        let value = headers[key]
+        request.setRequestHeader(key, value)
+      }
+      request.onreadystatechange = ()=>{
+        if(request.readyState === 4){
+          if(request.status >= 200 && request.status < 300){
+            resolve.call(undefined, request.responseText)
+          }else if(request.status >= 400){
+            reject.call(undefined, request)
+          }
+        }
+      }
+      request.send(body)
+    })
+  }
+  
+  myButton.addEventListener('click', (e)=>{
+    let promise = window.jQuery.ajax({
+      url: '/xxx',
+      method: 'get',
+      headers: {
+        'content-type':'application/x-www-form-urlencoded',
+        'frank': '18'
+      }
+    })
+  
+    promise.then(
+      (text)=>{console.log(text)},
+      (request)=>{console.log(request)}
+    )
+  
+  })
+  
